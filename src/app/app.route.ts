d27d619835718e274e0110bc339a651b6af8aa85
Lakshamana <guitrompa1@gmail.com>
2019-07-15 12:28:08 -0300

    modify configs

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -11,7 +11,7 @@ import { OrderSummaryComponent } from "./order/order-summary/order-summary.compo
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
     {path: 'about', component: AboutComponent},
-    {path: 'restaurants/:id', component: RestaurantDetailComponent, 
+    {path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
             {path: '', redirectTo: 'menu', pathMatch: 'full'},
             {path: 'menu', component: MenuComponent},
