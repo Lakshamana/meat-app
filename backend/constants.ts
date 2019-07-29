fc7f58bacd106767aaca9fec4ca99ff1428810cd
Lakshamana <guitrompa1@gmail.com>
2019-07-29 19:56:14 -0300

    add config and constants to backend

---
+++ backend/constants.ts
@@ -0,0 +1,13 @@
+export const messages = {
+  notAuthorized: 'Not Authorized.',
+  invalidData: 'Invalid data.',
+  needsAuth: 'You need to get authenticated.'
+}
+
+export const httpStatus = {
+  forbidden: 403,
+  unauthorized: 401,
+  notFound: 404,
+  ok: 200,
+}
+
