f4711d97f0a171198a68cac9115534d3a70d4d1c
Lakshamana <guitrompa1@gmail.com>
2019-07-30 16:10:21 -0300

    fix login page - whether to show signup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core'
+import { LoginService } from 'app/security/login/login.service'
 
 @Component({
   selector: 'mt-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  constructor(private loginService: LoginService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  isLoginRoute() {
+    const url = this.loginService.lastUrl
+    return url && url.startsWith('/login')
   }
-
 }
