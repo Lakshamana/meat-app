33cede7351905540f96cfe9fec3a7a49084717a2
Lakshamana <guitrompa1@gmail.com>
2019-07-30 22:25:22 -0300

    add AuthInterceptor

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http'
 
 import { ShoppingCartService } from 'app/restaurant-detail/shopping-cart/shopping-cart.service'
 import { CartItem } from 'app/restaurant-detail/shopping-cart/shopping-cart-item.model'
@@ -43,15 +43,8 @@ export class OrderService {
   }
 
   checkOrder(order: Order): Observable<string> {
-    let headers = new HttpHeaders()
-    if (this.loginService.isLoggedIn()) {
-      headers = headers.set(
-        'Authorization',
-        `Bearer ${this.loginService.user.accessToken}`
-      )
-    }
     return this.http
-      .post<Order>(`${MEAT_API}/orders`, order, { headers })
+      .post<Order>(`${MEAT_API}/orders`, order)
       .map(order => order.id)
   }
 }
